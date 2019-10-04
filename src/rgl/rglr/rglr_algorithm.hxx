582251fa92d4943b037e757ee82613a1ecf27ac6
Benjamin Yates <benjamin@rqdq.com>
2019-10-04 13:04:05 -0500

    avoid useless y+=0 for each quad during tile store

--- src/rgl/rglr/rglr_algorithm.hxx
+++ src/rgl/rglr/rglr_algorithm.hxx
@@ -23,33 +23,32 @@ void Filter(const QFloat4Canvas& src1, TrueColorCanvas& dst, const rmlg::irect r
 	using rmlv::qfloat2, rmlv::qfloat3, rmlv::qfloat4;
 	using rmlv::mvec4f, rmlv::mvec4i;
 
-	const float dw = 1.0F / dst.width();
-	const float dh = 1.0F / dst.height();
+	const float iw = 1.0F / dst.width();
+	const float ih = 1.0F / dst.height();
+	const mvec4f fcdx{  iw*2 };
+	const mvec4f fcdy{ -ih*2 };
+	mvec4f fcxLeft{(               rect.left.x + 0.5F) / float(dst.width()) };
+	fcxLeft += mvec4f{0,iw,0,iw};
+	mvec4f fcy    {(dst.height() - rect.top.y  - 0.5F) / float(dst.height())};
+	fcy -= mvec4f{0,0,ih,ih};
 
-	const qfloat2 dqx{ dw*2, 0 };
-	const qfloat2 dqy{ 0, -dh*2 };
-	qfloat2 q_row{
-		mvec4f{rect.left.x                     / float(dst.width()) } + mvec4f{0,dw,0,dw},
-		mvec4f{(dst.height() - 1 - rect.top.y) / float(dst.height())} - mvec4f{0,0,dh,dh}
-		};
-
-	for (int y = rect.top.y; y < rect.bottom.y; y += 2, q_row+=dqy) {
+	for (int y = rect.top.y; y < rect.bottom.y; y += 2, fcy+=fcdy) {
 		auto destRow1Addr = &dst.data()[y * dst.stride() + rect.left.x];
 		auto destRow2Addr = destRow1Addr + dst.stride();
 
 		auto* source1Addr = &src1.cdata()[(y >> 1) * (dst.width() >> 1) + (rect.left.x >> 1)];
 
-		qfloat2 q{ q_row };
+		mvec4f fcx = fcxLeft;
 		for (int x = rect.left.x; x < rect.right.x; x += 4, destRow1Addr+=4, destRow2Addr+=4) {
 			mvec4i packed[2];
-			for (int sub = 0; sub < 2; sub++, source1Addr++, q+=dqx) {
+			for (int sub = 0; sub < 2; sub++, source1Addr++, fcx+=fcdx) {
 
 				// load source1 color
 				qfloat3 sc;
 				QFloat4Canvas::Load(source1Addr, sc.x.v, sc.y.v, sc.z.v);
 
 				// shade & convert
-				qfloat3 fragColor = SHADER::ShadeCanvas(q, sc);
+				qfloat3 fragColor = SHADER::ShadeCanvas({ fcx, fcy }, sc);
 				packed[sub] = CONVERTER::to_tc(fragColor); }
 
 			_mm_stream_si128(reinterpret_cast<__m128i*>(destRow1Addr), _mm_unpacklo_epi64(packed[0].v, packed[1].v));
@@ -61,33 +60,32 @@ void Filter(const QShort3Canvas& src1, TrueColorCanvas& dst, const rmlg::irect r
 	using rmlv::qfloat2, rmlv::qfloat3, rmlv::qfloat4;
 	using rmlv::mvec4f, rmlv::mvec4i;
 
-	const float dw = 1.0F / dst.width();
-	const float dh = 1.0F / dst.height();
+	const float iw = 1.0F / dst.width();
+	const float ih = 1.0F / dst.height();
+	const mvec4f fcdx{  iw*2 };
+	const mvec4f fcdy{ -ih*2 };
+	mvec4f fcxLeft{(               rect.left.x + 0.5F) / float(dst.width()) };
+	fcxLeft += mvec4f{0,iw,0,iw};
+	mvec4f fcy    {(dst.height() - rect.top.y  - 0.5F) / float(dst.height())};
+	fcy -= mvec4f{0,0,ih,ih};
 
-	const qfloat2 dqx{ dw*2, 0 };
-	const qfloat2 dqy{ 0, -dh*2 };
-	qfloat2 q_row{
-		mvec4f{rect.left.x                     / float(dst.width()) } + mvec4f{0,dw,0,dw},
-		mvec4f{(dst.height() - 1 - rect.top.y) / float(dst.height())} - mvec4f{0,0,dh,dh}
-		};
-
-	for (int y = rect.top.y; y < rect.bottom.y; y += 2, q_row+=dqy) {
+	for (int y = rect.top.y; y < rect.bottom.y; y += 2, fcy+=fcdy) {
 		auto destRow1Addr = &dst.data()[y * dst.stride() + rect.left.x];
 		auto destRow2Addr = destRow1Addr + dst.stride();
 
 		auto* source1Addr = &src1.cdata()[(y >> 1) * (dst.width() >> 1) + (rect.left.x >> 1)];
 
-		qfloat2 q{ q_row };
+		mvec4f fcx = fcxLeft;
 		for (int x = rect.left.x; x < rect.right.x; x += 4, destRow1Addr+=4, destRow2Addr+=4) {
 			mvec4i packed[2];
-			for (int sub = 0; sub < 2; sub++, source1Addr++, q+=dqx) {
+			for (int sub = 0; sub < 2; sub++, source1Addr++, fcx+=fcdx) {
 
 				// load source1 color
 				qfloat3 source1Color;
 				QShort3Canvas::Load(source1Addr, source1Color.x.v, source1Color.y.v, source1Color.z.v);
 
 				// shade & convert
-				qfloat3 fragColor = SHADER::ShadeCanvas(q, source1Color);
+				qfloat3 fragColor = SHADER::ShadeCanvas({ fcx, fcy }, source1Color);
 				packed[sub] = CONVERTER::to_tc(fragColor); }
 
 			_mm_stream_si128(reinterpret_cast<__m128i*>(destRow1Addr), _mm_unpacklo_epi64(packed[0].v, packed[1].v));
