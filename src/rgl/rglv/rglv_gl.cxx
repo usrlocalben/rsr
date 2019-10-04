1280cac6a3464ee08af44a80dcad696e75de9cde
Benjamin Yates <benjamin@rqdq.com>
2019-10-04 14:19:30 -0500

    instances cont'd

--- src/rgl/rglv/rglv_gl.cxx
+++ src/rgl/rglv/rglv_gl.cxx
@@ -9,22 +9,43 @@
 namespace rqdq {
 namespace rglv {
 
-void GL::DrawArrays(const int mode, const int start, const int count, int instanceCnt) {
+void GL::DrawArrays(const int mode, const int start, const int count) {
 	assert(mode == GL_TRIANGLES);
 	assert(start == 0);
 	MaybeUpdateState();
-	commands_.appendByte(CMD_DRAW_ARRAY);
+	commands_.appendByte(CMD_DRAW_ARRAYS);
+	// commands_.appendByte(0x14);  // videocore: 16-bit indices, triangles
+	// commands_.appendByte(enableClipping ? 1 : 0);
+	commands_.appendInt(count); }
+
+
+void GL::DrawArraysInstanced(const int mode, const int start, const int count, int instanceCnt) {
+	assert(mode == GL_TRIANGLES);
+	assert(start == 0);
+	MaybeUpdateState();
+	commands_.appendByte(CMD_DRAW_ARRAYS_INSTANCED);
 	// commands_.appendByte(0x14);  // videocore: 16-bit indices, triangles
 	// commands_.appendByte(enableClipping ? 1 : 0);
 	commands_.appendInt(count);
 	commands_.appendInt(instanceCnt); }
 
 
-void GL::DrawElements(const int mode, const int count, const int type, const uint16_t* indices, int instanceCnt) {
+void GL::DrawElements(const int mode, const int count, const int type, const uint16_t* indices) {
 	assert(mode == GL_TRIANGLES);
 	assert(type == GL_UNSIGNED_SHORT);
 	MaybeUpdateState();
 	commands_.appendByte(CMD_DRAW_ELEMENTS);
+	commands_.appendByte(0x14);  // videocore: 16-bit indices, triangles
+	// commands_.appendByte(enableClipping ? 1 : 0);
+	commands_.appendInt(count);
+	commands_.appendPtr(indices); }
+
+
+void GL::DrawElementsInstanced(const int mode, const int count, const int type, const uint16_t* indices, int instanceCnt) {
+	assert(mode == GL_TRIANGLES);
+	assert(type == GL_UNSIGNED_SHORT);
+	MaybeUpdateState();
+	commands_.appendByte(CMD_DRAW_ELEMENTS_INSTANCED);
 	commands_.appendByte(0x14);  // videocore: 16-bit indices, triangles
 	// commands_.appendByte(enableClipping ? 1 : 0);
 	commands_.appendInt(count);
