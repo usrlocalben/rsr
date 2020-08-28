usrlocalben/rsr

commit 547b987e590cd506706125bc6de1f724c3e071b7

various rasterization details
- get unittest working again
- fix test checking uv interpolation on pixel centers
- use rcp+NR for 1/x
- adjust for half-space over pixel centers in fixed point

src/rgl/rglv/rglv_triangle.hxx
@@ -35,9 +35,17 @@ struct BlahFp {
 	auto Render(rmlv::qfloat2 frag_coord, rmlv::mvec4i trimask, rglv::BaryCoord BS, bool frontfacing) -> void {}; };
 */
 
+#if 1
 constexpr int FP_BITS = 8;
 constexpr int FP_MASK = 0xff;
 constexpr float FP_MUL = 256.0F;
+constexpr int FP_HALF = 1<<(FP_BITS-1);
+#else
+constexpr int FP_BITS = 4;
+constexpr int FP_MASK = 0xf;
+constexpr float FP_MUL = 16.0F;
+constexpr int FP_HALF = 1<<(FP_BITS-1);
+#endif
 
 
 template <bool SCISSOR_TEST, typename FRAGMENT_PROCESSOR>
@@ -59,12 +67,13 @@ public:
 
 	// MANIPULATORS
 	void Draw(rmlv::vec4 s1, rmlv::vec4 s2, rmlv::vec4 s3, const bool frontfacing) {
-		int x1 = int(FP_MUL * (s1.x - 0.5f));
-		int x2 = int(FP_MUL * (s2.x - 0.5f));
-		int x3 = int(FP_MUL * (s3.x - 0.5f));
-		int y1 = int(FP_MUL * (s1.y - 0.5f));
-		int y2 = int(FP_MUL * (s2.y - 0.5f));
-		int y3 = int(FP_MUL * (s3.y - 0.5f));
+		constexpr float co = 0.0F;  // pixel-center correction offset
+		int x1 = int(FP_MUL * (s1.x - co));
+		int x2 = int(FP_MUL * (s2.x - co));
+		int x3 = int(FP_MUL * (s3.x - co));
+		int y1 = int(FP_MUL * (s1.y - co));
+		int y2 = int(FP_MUL * (s2.y - co));
+		int y3 = int(FP_MUL * (s3.y - co));
 		Draw(x1, x2, x3, y1, y2, y3, frontfacing); }
 
 	void Draw(int x1, int x2, int x3, int y1, int y2, int y3, const bool frontfacing) {
@@ -74,27 +83,34 @@ public:
 
 		const int q = 2; // block size is 2x2
 
-		int minx = max((rmlv::Min(x1, x2, x3) + FP_MASK) >> FP_BITS, rect_.left.x);
-		int endx = min((rmlv::Max(x1, x2, x3) + FP_MASK) >> FP_BITS, rect_.right.x);
-		int miny = max((rmlv::Min(y1, y2, y3) + FP_MASK) >> FP_BITS, rect_.top.y);
-		int endy = min((rmlv::Max(y1, y2, y3) + FP_MASK) >> FP_BITS, rect_.bottom.y);
+		auto Ceil = [](int x) -> int {
+			return (x + FP_MASK) >> FP_BITS; };
+
+		int minx = max(Ceil(rmlv::Min(x1, x2, x3)), rect_.left.x);
+		int endx = min(Ceil(rmlv::Max(x1, x2, x3)), rect_.right.x);
+		int miny = max(Ceil(rmlv::Min(y1, y2, y3)), rect_.top.y);
+		int endy = min(Ceil(rmlv::Max(y1, y2, y3)), rect_.bottom.y);
 		minx &= ~(q - 1); // align to 2x2 block
 		miny &= ~(q - 1);
 
-		int64_t dx12 = x1 - x2, dy12 = y2 - y1;
-		int64_t dx23 = x2 - x3, dy23 = y3 - y2;
-		int64_t dx31 = x3 - x1, dy31 = y1 - y3;
+		int64_t dx12 = x1-x2, dy12 = y2-y1;
+		int64_t dx23 = x2-x3, dy23 = y3-y2;
+		int64_t dx31 = x3-x1, dy31 = y1-y3;
 
 		// setup edge values at the starting point
-		int64_t c1 = dy12*((minx<<FP_BITS) - x1) + dx12*((miny<<FP_BITS)-y1);
-		int64_t c2 = dy23*((minx<<FP_BITS) - x2) + dx23*((miny<<FP_BITS)-y2);
-		int64_t c3 = dy31*((minx<<FP_BITS) - x3) + dx31*((miny<<FP_BITS)-y3);
+		int64_t sx = minx<<FP_BITS;
+		int64_t sy = miny<<FP_BITS;
+		const int64_t offset = FP_HALF;
+		int64_t c1 = dy12*(sx-x1+offset) + dx12*(sy-y1+offset);
+		int64_t c2 = dy23*(sx-x2+offset) + dx23*(sy-y2+offset);
+		int64_t c3 = dy31*(sx-x3+offset) + dx31*(sy-y3+offset);
 
 		// correct for top-left fill
+#if 1
 		if (dy12 > 0 || (dy12==0 && dx12>0)) c1++;  --c1;
 		if (dy23 > 0 || (dy23==0 && dx23>0)) c2++;  --c2;
 		if (dy31 > 0 || (dy31==0 && dx31>0)) c3++;  --c3;
-
+#endif
 		c1 >>= FP_BITS;
 		c2 >>= FP_BITS;
 		c3 >>= FP_BITS;
@@ -102,11 +118,14 @@ public:
 		auto cb1 = mvec4i(c1) + mvec4i(0,dy12,0,dy12) + mvec4i(0,0,dx12,dx12);
 		auto cb2 = mvec4i(c2) + mvec4i(0,dy23,0,dy23) + mvec4i(0,0,dx23,dx23);
 		auto cb3 = mvec4i(c3) + mvec4i(0,dy31,0,dy31) + mvec4i(0,0,dx31,dx31);
+		// std::cerr << "dydx(" << dy12 << ", " << dy23 << ", " << dy31 << ")\n";
+		// std::exit(1);
 		auto cb1dydx = mvec4i(dy12*2); auto cb1dxdy = mvec4i(dx12*2);
 		auto cb2dydx = mvec4i(dy23*2); auto cb2dxdy = mvec4i(dx23*2);
 		auto cb3dydx = mvec4i(dy31*2); auto cb3dxdy = mvec4i(dx31*2);
 
 		mvec4f scale{1.0f / (c1 + c2 + c3)};
+		// std::cerr << "scale(" << scale.get_x() << ")\n";
 
 		program_.Begin(minx, miny);
 		for (int y=miny; y<endy; y+=2, cb1+=cb1dxdy, cb2+=cb2dxdy, cb3+=cb3dxdy, program_.CR()) {

src/rgl/rglr/rglr_algorithm.cxx
@@ -27,7 +27,7 @@ void Fill(QFloatCanvas& dst, const float value, const rmlg::irect rect) {
 
 // xxx this Fill ignores _rect_ and assumes that the canvas _dst_
 // is a tile
-void FillAll(QFloatCanvas& dst, const float value) { //, const rmlg::irect) {
+void Fill(QFloatCanvas& dst, const float value) { //, const rmlg::irect) {
 	const __m128 vvvv   = _mm_set1_ps(value);
 
 	auto dstLeft = dst.data();
@@ -41,7 +41,7 @@ void FillAll(QFloatCanvas& dst, const float value) { //, const rmlg::irect) {
 			QFloatCanvas::Store(vvvv, p); }}}
 
 
-void FillAll(QFloat4Canvas& dst, const rmlv::vec4 value) { //, const rmlg::irect) {
+void Fill(QFloat4Canvas& dst, const rmlv::vec4 value) { //, const rmlg::irect) {
 	const __m128 red   = _mm_set1_ps(value.x);
 	const __m128 green = _mm_set1_ps(value.y);
 	const __m128 blue  = _mm_set1_ps(value.z);
@@ -58,7 +58,7 @@ void FillAll(QFloat4Canvas& dst, const rmlv::vec4 value) { //, const rmlg::irect
 			QFloat4Canvas::Store(red, green, blue, alpha, p); }}}
 
 
-void FillAll(QFloat3Canvas& dst, const rmlv::vec3 value) { //, const rmlg::irect) {
+void Fill(QFloat3Canvas& dst, const rmlv::vec3 value) { //, const rmlg::irect) {
 	const __m128 red   = _mm_set1_ps(value.x);
 	const __m128 green = _mm_set1_ps(value.y);
 	const __m128 blue  = _mm_set1_ps(value.z);

src/rgl/rglr/rglr_algorithm.hxx
@@ -9,9 +9,9 @@
 namespace rqdq {
 namespace rglr {
 
-void FillAll(QFloatCanvas& dst, float value);
-void FillAll(QFloat3Canvas& dst, rmlv::vec3 value);
-void FillAll(QFloat4Canvas& dst, rmlv::vec4 value);
+void Fill(QFloatCanvas& dst, float value);
+void Fill(QFloat3Canvas& dst, rmlv::vec3 value);
+void Fill(QFloat4Canvas& dst, rmlv::vec4 value);
 
 void Fill(QFloatCanvas& dst, float value, rmlg::irect rect);
 void Fill(QFloat4Canvas& dst, rmlv::vec4 value, rmlg::irect rect);

src/rgl/rglv/rglv_gpu.cxx
@@ -301,17 +301,17 @@ void GPU::DrawImpl(const unsigned tid, const int tileIdx) {
 					std::exit(1); }
 				const auto fillValue = rmlv::vec4{ stateptr->clearColor.xyz(), stateptr->clearDepth };
 				rglr::QFloat4Canvas cc{ tileDimensionsInPixels_.x, tileDimensionsInPixels_.y, static_cast<rmlv::qfloat4*>(color0BufPtr), 64 };
-				FillAll(cc, fillValue); }
+				Fill(cc, fillValue); }
 			else {
 				if ((bits&GL_COLOR_BUFFER_BIT) != 0) {
 					if (stateptr->color0AttachmentType == RB_RGBF32) {
 						const auto fillValue = stateptr->clearColor.xyz();
 						rglr::QFloat3Canvas cc{ tileDimensionsInPixels_.x, tileDimensionsInPixels_.y, static_cast<rmlv::qfloat3*>(color0BufPtr), 64 };
-						FillAll(cc, fillValue); }
+						Fill(cc, fillValue); }
 					else if (stateptr->color0AttachmentType == RB_RGBAF32) {
 						const auto fillValue = stateptr->clearColor;
 						rglr::QFloat4Canvas cc{ tileDimensionsInPixels_.x, tileDimensionsInPixels_.y, static_cast<rmlv::qfloat4*>(color0BufPtr), 64 };
-						FillAll(cc, fillValue); }
+						Fill(cc, fillValue); }
 					else {
 						std::cerr << "can't clear color buffer of type RB " << stateptr->color0AttachmentType << "\n";
 						std::exit(1); }}
@@ -319,7 +319,7 @@ void GPU::DrawImpl(const unsigned tid, const int tileIdx) {
 					if (stateptr->depthAttachmentType == RB_F32) {
 						const auto fillValue = stateptr->clearDepth;
 						rglr::QFloatCanvas cc{ tileDimensionsInPixels_.x, tileDimensionsInPixels_.y, static_cast<rmlv::qfloat*>(depthBufPtr), 64 };
-						FillAll(cc, fillValue); }
+						Fill(cc, fillValue); }
 					else {
 						std::cerr << "can't clear depth buffer of type RB " << stateptr->depthAttachmentType << "\n";
 						std::exit(1); }}}}

src/rgl/rglv/rglv_gpu.hxx
@@ -604,7 +604,7 @@ protected:
 		auto vsz = s.viewportSize.value_or(bufferDimensionsInPixels_);
 		auto vpx = s.viewportOrigin;
 		auto DS = rmlv::qfloat2( vsz.x/2, -vsz.y/2 );
-		auto DO = rmlv::qfloat2( vpx.x, bufferDimensionsInPixels_.y-vpx.y );
+		auto DO = rmlv::qfloat2( vsz.x/2+vpx.x, bufferDimensionsInPixels_.y-(vsz.y/2+vpx.y) );
 		return { DS, DO }; }
 
 	auto ScissorRect(const GLState& s) const -> rmlg::irect {
@@ -747,7 +747,7 @@ class GPUBinImpl : GPU {
 				auto flags = frustum.Test(coord);
 				store_bytes(clipFlagBuffer_.data() + vi, flags);
 
-				auto devCoord = (pdiv(coord).xy() + _1) * DS + DO;
+				auto devCoord = pdiv(coord).xy() * DS + DO;
 				devCoord.template store<false>(devCoordXBuffer_.data() + vi,
 				                               devCoordYBuffer_.data() + vi); }
 
@@ -861,7 +861,7 @@ class GPUBinImpl : GPU {
 					qfloat4 gl_Position;
 					SHADER::ShadeVertex(matrices, uniforms, vertex[i], gl_Position, computed[i]);
 					clipFlags[i] = frustum.Test(gl_Position);
-					devCoord[i] = (pdiv(gl_Position).xy() + _1) * DS + DO;
+					devCoord[i] = pdiv(gl_Position).xy() * DS + DO;
 					/* todo compute 4x triangle area here */ }
 
 				for (int ti{0}; ti<li; ++ti) {
@@ -1020,8 +1020,8 @@ class GPUBinImpl : GPU {
 			for (auto& vertex : clipA_) {
 				// convert clip-coord to device-coord
 				vertex.coord = pdiv(vertex.coord);
-				vertex.coord.x = ((vertex.coord.x+1) * DS.x + DO.x).get_x();
-				vertex.coord.y = ((vertex.coord.y+1) * DS.y + DO.y).get_x(); }
+				vertex.coord.x = (vertex.coord.x * DS.x + DO.x).get_x();
+				vertex.coord.y = (vertex.coord.y * DS.y + DO.y).get_x(); }
 			// end of phase 2: poly contains a clipped N-gon
 
 			// check direction, maybe cull, maybe reorder
@@ -1139,8 +1139,8 @@ class GPUTileImpl : GPU {
 				qfloat4 gl_Position;
 				SHADER::ShadeVertex(matrices, uniforms, vertex[i], gl_Position, computed[i]);
 				devCoord[i] = pdiv(gl_Position);
-				fx[i] = ftoi(rglv::FP_MUL * ((devCoord[i].x+_1) * DS.x + DO.x));
-				fy[i] = ftoi(rglv::FP_MUL * ((devCoord[i].y+_1) * DS.y + DO.y)); }
+				fx[i] = ftoi(rglv::FP_MUL * (devCoord[i].x * DS.x + DO.x));
+				fy[i] = ftoi(rglv::FP_MUL * (devCoord[i].y * DS.y + DO.y)); }
 
 			// draw up to 4 triangles
 			for (int ti=0; ti<li; ti++) {

src/rgl/rglv/rglv_triangle.t.cxx
@@ -264,7 +264,7 @@ bool check_triangle(array<vec4, 3> points, array<string, 8> expected) {
 	rglr::QFloat4Canvas cbc(8, 8);
 	rglr::QFloatCanvas dbc(8, 8);
 
-	rglr::Fill(cbc, vec4{ 0, 0, 0, 1.0F }, cbc.rect());
+	rglr::Fill(cbc, vec4{ 0, 0, 0, 1.0F });
 
 	bool frontfacing = true;
 
@@ -275,7 +275,7 @@ bool check_triangle(array<vec4, 3> points, array<string, 8> expected) {
 		DebugWithBary::VertexOutputSD{},
 		DebugWithBary::VertexOutputSD{},
 		DebugWithBary::VertexOutputSD{});
-	rglv::TriangleRasterizer tr(target_program, cbc.rect(), cbc.height());
+	rglv::TriangleRasterizer<false, decltype(target_program)> tr(target_program, cbc.rect(), cbc.height());
 	tr.Draw(points[0], points[1], points[2], frontfacing);
 
 	// compare
@@ -296,28 +296,31 @@ bool check_triangle(array<vec4, 3> points, array<string, 8> expected) {
  * verify that the target is filled with the correct u,v values within 0.01f
  */
 bool check_triangle_uv() {
-	/*const int width = 2560;
-	const int height = 2560;
-	const float fwidth = 2560.0f;
-	const float fheight = 2560.0f;
-	const float right = 2560.5f;
-	const float bottom = 2560.5f;*/
-	const int width = 16;
-	const int height = 16;
-	const float fwidth = 16.0F;
-	const float fheight = 16.0F;
-	const float right = 16.5F;
-	const float bottom = 16.5F;
+#if 0
+	const int width = 256;
+	const int height = 256;
+	const float fwidth = 256.0f;
+	const float fheight = 256.0f;
+	const float right = 256.5f;
+	const float bottom = 256.5f;
+#else
+	const int dim = 2560;
+	const int w = dim;
+	const int h = dim;
+#endif
 
-	rglr::QFloat4Canvas cbc(width, height);
-	rglr::QFloatCanvas dbc(width, height);
+	rglr::QFloat4Canvas cbc(w, h);
+	rglr::QFloatCanvas dbc(w, h);
 
-	rglr::Fill(cbc, vec4{ 0, 0, 0, 1.0F }, cbc.rect());
+	rglr::Fill(cbc, vec4{ 0, 0, 0, 1.0F });
 
 	bool frontfacing = true;
 
-	vec4 ulp{ 0.5, 0.5, 0, 1 };     vec2 uluv{ 0.0, fheight };  vec4 urp{ right,    0.5, 0, 1 };  vec2 uruv{ fwidth, fheight };
-	vec4 llp{ 0.5, bottom, 0, 1 };  vec2 lluv{ 0.0,    0.0 };  vec4 lrp{ right, bottom, 0, 1 };  vec2 lruv{ fwidth,   0.0 };
+	vec4 ulp( 0, 0, 0, 1 ); vec4 urp( w, 0, 0, 1 );
+	vec4 llp( 0, h, 0, 1 ); vec4 lrp( w, h, 0, 1 );
+
+	vec2 uluv( 0, h ); vec2 uruv( w, h );
+	vec2 lluv( 0, 0 ); vec2 lruv( w, 0 );
 
 	array<vec4, 3> points_upper_left = { ulp, llp, urp, };
 	array<vec2, 3> uv_upper_left = { uluv, lluv, uruv, };
@@ -335,7 +338,7 @@ bool check_triangle_uv() {
 			VertexFloat1{ points[0].w, points[1].w, points[2].w },
 			VertexFloat1{ points[0].z, points[1].z, points[2].z },
 			computed1, computed2, computed3);
-		rglv::TriangleRasterizer tr(target_program, cbc.rect(), cbc.height());
+		rglv::TriangleRasterizer<false, decltype(target_program)> tr(target_program, cbc.rect(), cbc.height());
 		tr.Draw(points[0], points[1], points[2], frontfacing); }
 	{
 		auto& points = points_lower_right;
@@ -348,31 +351,33 @@ bool check_triangle_uv() {
 			VertexFloat1{ points[0].w, points[1].w, points[2].w },
 			VertexFloat1{ points[0].z, points[1].z, points[2].z },
 			computed1, computed2, computed3);
-		rglv::TriangleRasterizer tr(target_program, cbc.rect(), cbc.height());
+		rglv::TriangleRasterizer<false, decltype(target_program)> tr(target_program, cbc.rect(), cbc.height());
 		tr.Draw(points[0], points[1], points[2], frontfacing); }
+
+	auto almostEqual = [](float a, float b) {
+		constexpr float ep = 0.01F;
+		return (b-ep < a && a < b+ep); };
 
 
 	// cout << "=========== data ============\n";
-	const float expected_accuracy = 0.01F;
-	for (int yy = 0; yy < height; yy++) {
-		for (int xx = 0; xx < width; xx++) {
+	for (int yy = 0; yy < h; yy++) {
+		for (int xx = 0; xx < w; xx++) {
 			auto px = cbc.get_pixel(xx, yy);
 
-			int iu = int(px.x + expected_accuracy);
-			int iv = int(px.y + expected_accuracy);
+			// expected values
+			auto ex =   xx   + 0.5F;
+			auto ey = (h-yy) - 0.5F;
 
-			//float dx = float(xx) - px.x;
-			//float dy = float(height - yy) - px.y;
-			//cout << "(" << dx << "," << dy << ") ";
-			// cout << "(" << int(px.x+0.01f) << "," << int(px.y+0.01f) << ") ";
-			if (iu != xx) {
-				// verify U is 0,1,2,3,4,5,6,7 from lef to right
+			// cerr << "xx(" << xx << ") yy(" << yy << ") px(" << px.x << ", " << px.y << ") expected(" << ex << ", " << ey << ")\n";
+
+			if (!almostEqual(px.x, ex)) {
+				// verify U is [0.5 1.5, 2.5, ... w-0.5] left to right
 				return false; }
-			if (iv != (height - yy)) {
-				// verify V is 8,7,6,5,4,3,2,1 from top to bottom
+			if (!almostEqual(px.y, ey)) {
+				// verify V is [h-0.5, ... 2.5, 1.5, 0.5] top to bottom
 				return false; }
 		}
-		//cout << "\n";
+		//cerr << "\n";
 	}
 	return true; }
 

src/rml/rmlv/rmlv_mvec4.hxx
@@ -578,7 +578,25 @@ auto select_by_sign(mvec4f a, mvec4f b, mvec4f mask) -> mvec4f {
 
 inline
 auto oneover(mvec4f a) -> mvec4f {
-	return _mm_rcp_ps(a.v); }
+	__m128 res;
+
+	// IEEE 
+#if 0
+	res = _mm_div_ps(_mm_set1_ps(1.0F), a.v);
+#endif
+
+	// SSE approx
+#if 0
+	res = _mm_rcp_ps(a.v);
+#endif
+
+	// approx + one NR iter
+#if 1
+	res = _mm_rcp_ps(a.v);
+	__m128 muls = _mm_mul_ps(a.v, _mm_mul_ps(res, res));
+	res = _mm_sub_ps(_mm_add_ps(res, res), muls);
+#endif
+	return res; }
 
 inline
 auto pow(mvec4f x, mvec4f y) -> mvec4f {

commit 9028e62bf47e722747a62cc475b256ed0bf07b20

fix regression caused by 547b987
upper-left pixel position incorrect after the pixel-center changes

src/rgl/rglv/rglv_triangle.hxx
@@ -85,11 +85,13 @@ public:
 
 		auto Ceil = [](int x) -> int {
 			return (x + FP_MASK) >> FP_BITS; };
+		auto Floor = [](int x) -> int {
+			return x >> FP_BITS; };
 
-		int minx = max(Ceil(rmlv::Min(x1, x2, x3)), rect_.left.x);
-		int endx = min(Ceil(rmlv::Max(x1, x2, x3)), rect_.right.x);
-		int miny = max(Ceil(rmlv::Min(y1, y2, y3)), rect_.top.y);
-		int endy = min(Ceil(rmlv::Max(y1, y2, y3)), rect_.bottom.y);
+		int minx = max(Floor(rmlv::Min(x1, x2, x3)), rect_.left.x);
+		int endx = min( Ceil(rmlv::Max(x1, x2, x3)), rect_.right.x);
+		int miny = max(Floor(rmlv::Min(y1, y2, y3)), rect_.top.y);
+		int endy = min( Ceil(rmlv::Max(y1, y2, y3)), rect_.bottom.y);
 		minx &= ~(q - 1); // align to 2x2 block
 		miny &= ~(q - 1);
 
@@ -98,12 +100,12 @@ public:
 		int64_t dx31 = x3-x1, dy31 = y1-y3;
 
 		// setup edge values at the starting point
-		int64_t sx = minx<<FP_BITS;
-		int64_t sy = miny<<FP_BITS;
-		const int64_t offset = FP_HALF;
-		int64_t c1 = dy12*(sx-x1+offset) + dx12*(sy-y1+offset);
-		int64_t c2 = dy23*(sx-x2+offset) + dx23*(sy-y2+offset);
-		int64_t c3 = dy31*(sx-x3+offset) + dx31*(sy-y3+offset);
+		// offset by FP_HALF to evaluate pixel centers
+		int64_t sx = (minx<<FP_BITS) + FP_HALF;
+		int64_t sy = (miny<<FP_BITS) + FP_HALF;
+		int64_t c1 = dy12*(sx-x1) + dx12*(sy-y1);
+		int64_t c2 = dy23*(sx-x2) + dx23*(sy-y2);
+		int64_t c3 = dy31*(sx-x3) + dx31*(sy-y3);
 
 		// correct for top-left fill
 #if 1